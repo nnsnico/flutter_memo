338c131897bc76e62588963a3834d236643babbe
nnsnico <yuji.jampan.blue@gmail.com>
2024-08-05 00:16:35 +0900

    chore: tweak all tests and config of fvm

--- data/datastore/test/note_datasource_test.dart
+++ data/datastore/test/note_datasource_test.dart
@@ -1,7 +1,6 @@
 import 'package:datastore/note_datasource.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:model/note.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 Future<void> main() async {
